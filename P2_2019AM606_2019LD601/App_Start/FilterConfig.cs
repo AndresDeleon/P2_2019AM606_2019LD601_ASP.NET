d3f80f975a028edee43316c96f3d70a44c38b5ff
Andrés Enrique Lemus Deleón <andres.lemus@catolica.edu.sv>
2022-04-30 12:19:05 -0600

    Primera Carga

---
+++ P2_2019AM606_2019LD601/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace P2_2019AM606_2019LD601
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ P2_2019AM606_2019LD601/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="P2_2019AM606_2019LD601.MvcApplication" Language="C#" %>
